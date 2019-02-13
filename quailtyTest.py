3377d61c30e9e959d1e582326e3caaed0bfefa76
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2019-02-12 20:52:37 -0500

    Configuring VS Code and pyLint

--- quailtyTest.py
+++ quailtyTest.py
@@ -1,6 +1,8 @@
+# takes the same photo with various encodings to compare quality
+
 # import APIs for piCamera and timings
 from time import sleep
 from picamera import piCamera
 
-#define camera as the device on CSI port 0
+# define camera as the device on CSI port 0
 camera = piCamera()
